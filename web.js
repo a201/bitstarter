b5abf4058178502f6f1e16cc307dfad67cc64cf0
Jose Santiago <jsantiago@fastmail.us>
2013-07-09 21:00:24 -0500

    Added untested changes to web.js

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var file_contents = fs.readFileSync("index.html");
+var content_string = file_contents.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(content_string);
 });
 
 var port = process.env.PORT || 5000;
