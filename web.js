d030fd77f768d3423f74494f6adefdb679483ec8
Jose Santiago <jsantiago@fastmail.us>
2013-07-09 21:07:31 -0500

    Added slighly more tested changes to web.js

--- web.js
+++ web.js
@@ -2,14 +2,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs=require('fs');
 var file_contents = fs.readFileSync("index.html");
 var content_string = file_contents.toString();
 
+//console.log(content_string);
 app.get('/', function(request, response) {
   response.send(content_string);
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+    app.listen(port, function() {
+   console.log("Listening on " + port);
 });
